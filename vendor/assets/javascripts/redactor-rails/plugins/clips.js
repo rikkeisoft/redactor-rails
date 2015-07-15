6e24d1c7abd2c946c4968bae65f1f4592c6347e5
HuyNQ <huy@huynq.net>
2015-07-15 08:50:43 +0700

    Bump to 0.5.5

--- vendor/assets/javascripts/redactor-rails/plugins/clips.js
+++ vendor/assets/javascripts/redactor-rails/plugins/clips.js
@@ -1,65 +1,68 @@
-(function($)
-{
-	$.Redactor.prototype.clips = function()
-	{
-		return {
-			init: function()
-			{
-			      var templates = REDACTOR.params.clips;
-			      if templates === undefined {
-                              templates = [];
-			      }
-				var items = [
-					templates
-				];
+(function($) {
+  $.Redactor.prototype.clips = function() {
+    return {
+      init: function() {
+        var items = [],
+              templates = undefined;
+        if (this.opts.template_type == 'summary' && REDACTOR.params.clips.summary) {
+          templates = REDACTOR.params.clips.summary;
+        } else if (this.opts.template_type == 'detail' && REDACTOR.params.clips.detail) {
+          templates = REDACTOR.params.clips.detail;
+        }
+        if (templates !== undefined && templates.length > 0) {
+          items = templates;
+        }
 
-				this.clips.template = $('<ul id="redactor-modal-list">');
+        this.clips.template = $('<ul id="redactor-modal-list">');
 
-				for (var i = 0; i < items.length; i++)
-				{
-					var li = $('<li>');
-					var a = $('<a href="#" class="redactor-clip-link">').text(items[i][0]);
-					var div = $('<div class="redactor-clip">').hide().html(items[i][1]);
+        if (items.length > 0) {
+          for (var i = 0; i < items.length; i++) {
+            var li = $('<li>');
+            var a = $('<a href="#" class="redactor-clip-link">').text(items[i][0]);
+            var div = $('<div class="redactor-clip">').hide().html(items[i][1]);
 
-					li.append(a);
-					li.append(div);
-					this.clips.template.append(li);
-				}
+            li.append(a);
+            li.append(div);
+            this.clips.template.append(li);
+          }
+        } else {
+          var li = $('<li>'),
+                span = $('<span>');
+          span.text(this.lang.get('clip_no_template'));
+          li.append(span)
+          this.clips.template.append(li);
+        }
 
-				this.modal.addTemplate('clips', '<section>' + this.utils.getOuterHtml(this.clips.template) + '</section>');
+        this.modal.addTemplate('clips', '<section>' + this.utils.getOuterHtml(this.clips.template) + '</section>');
 
-				var button = this.button.add('clips', this.lang.get('clip'));
-				this.button.addCallback(button, this.clips.show);
+        var button = this.button.add('clips', this.lang.get('clip_label'));
+        this.button.addCallback(button, this.clips.show);
 
-			},
-			show: function()
-			{
-				this.modal.load('clips', this.lang.get('clip_insert'), 400);
+      },
+      show: function() {
+        this.modal.load('clips', this.lang.get('clip_insert'), 400);
 
-				this.modal.createCancelButton();
+        this.modal.createCancelButton();
 
-				$('#redactor-modal-list').find('.redactor-clip-link').each($.proxy(this.clips.load, this));
+        $('#redactor-modal-list').find('.redactor-clip-link').each($.proxy(this.clips.load, this));
 
-				this.selection.save();
-				this.modal.show();
-			},
-			load: function(i,s)
-			{
-				$(s).on('click', $.proxy(function(e)
-				{
-					e.preventDefault();
-					this.clips.insert($(s).next().html());
+        this.selection.save();
+        this.modal.show();
+      },
+      load: function(i,s) {
+        $(s).on('click', $.proxy(function(e) {
+          e.preventDefault();
+          this.clips.insert($(s).next().html());
 
-				}, this));
-			},
-			insert: function(html)
-			{
-				this.selection.restore();
-				this.insert.html(html);
-				this.modal.close();
-				this.observe.load();
-			}
-		};
-	};
+        }, this));
+      },
+      insert: function(html) {
+        this.selection.restore();
+        this.insert.html(html);
+        this.modal.close();
+        this.observe.load();
+      }
+    };
+  };
 })(jQuery);
 
